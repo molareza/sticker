ff6eeb311389f4e11f41b5ca09bf73a13191d21c
Ruben Gees <rubengees7@googlemail.com>
2017-02-22 10:03:30 +0100

    Make the EmojiEditText and EmojiTextView more performant (#93)
    Thanks once again 👍

--- emoji/src/main/java/com/vanniktech/emoji/EmojiHandler.java
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiHandler.java
@@ -3,32 +3,82 @@ package com.vanniktech.emoji;
 import android.content.Context;
 import android.text.Spannable;
 import com.vanniktech.emoji.emoji.Emoji;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.vanniktech.emoji.EmojiHandler.SpanRangeList.SPAN_NOT_FOUND;
 
 final class EmojiHandler {
   static void addEmojis(final Context context, final Spannable text, final int emojiSize) {
-    final EmojiSpan[] spans = text.getSpans(0, text.length(), EmojiSpan.class);
+    final SpanRangeList existingSpanRanges = new SpanRangeList(text);
+    final EmojiManager emojiManager = EmojiManager.getInstance();
+    int index = 0;
 
-    for (final EmojiSpan oldSpan : spans) {
-      text.removeSpan(oldSpan);
-    }
+    while (index < text.length()) {
+      final int existingSpanEnd = existingSpanRanges.spanEnd(index);
 
-    int i = 0;
+      if (existingSpanEnd == SPAN_NOT_FOUND) {
+        final int nextSpanStart = existingSpanRanges.nextSpanStart(index);
+        final int searchRange = nextSpanStart == SPAN_NOT_FOUND ? text.length() : nextSpanStart;
+        final Emoji found = emojiManager.findEmoji(text.subSequence(index, searchRange));
 
-    final EmojiManager instance = EmojiManager.getInstance();
+        if (found != null) {
+          text.setSpan(new EmojiSpan(context, found.getResource(), emojiSize), index, index + found.getLength(),
+                  Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-    while (i < text.length()) {
-      final Emoji found = instance.findEmoji(text.subSequence(i, text.length()));
-
-      if (found != null) {
-        text.setSpan(new EmojiSpan(context, found.getResource(), emojiSize), i, i + found.getLength(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-        i += found.getLength();
+          index += found.getLength();
+        } else {
+          index++;
+        }
       } else {
-        i++;
+        index += existingSpanEnd - index;
       }
     }
   }
 
   private EmojiHandler() {
     throw new AssertionError("No instances.");
+  }
+
+  static final class SpanRangeList {
+    static final int SPAN_NOT_FOUND = -1;
+
+    private final List<Range> spanRanges = new ArrayList<>();
+
+    SpanRangeList(final Spannable text) {
+      for (final EmojiSpan span : text.getSpans(0, text.length(), EmojiSpan.class)) {
+        spanRanges.add(new Range(text.getSpanStart(span), text.getSpanEnd(span)));
+      }
+    }
+
+    int spanEnd(final int index) {
+      for (final Range spanRange : spanRanges) {
+        if (spanRange.start == index) {
+          return spanRange.end;
+        }
+      }
+
+      return SPAN_NOT_FOUND;
+    }
+
+    int nextSpanStart(final int index) {
+      for (final Range spanRange : spanRanges) {
+        if (spanRange.start > index) {
+          return spanRange.start;
+        }
+      }
+
+      return SPAN_NOT_FOUND;
+    }
+  }
+
+  static final class Range {
+    final int start;
+    final int end;
+
+    Range(final int start, final int end) {
+      this.start = start;
+      this.end = end;
+    }
   }
 }
