aa13afd20ce114cc4cdc59f8f23de12df72a3de3
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-06-01 21:00:42 +0200

    Add night theme support to sample and library. (#134)

--- app/src/main/java/com/vanniktech/emoji/sample/EmojiApplication.java
+++ app/src/main/java/com/vanniktech/emoji/sample/EmojiApplication.java
@@ -2,9 +2,12 @@ package com.vanniktech.emoji.sample;
 
 import android.app.Application;
 import android.os.StrictMode;
+import android.support.v7.app.AppCompatDelegate;
 import com.squareup.leakcanary.LeakCanary;
 import com.vanniktech.emoji.EmojiManager;
 import com.vanniktech.emoji.ios.IosEmojiProvider;
+
+import static android.support.v7.app.AppCompatDelegate.MODE_NIGHT_AUTO;
 
 public class EmojiApplication extends Application {
   @Override public void onCreate() {
@@ -13,6 +16,8 @@ public class EmojiApplication extends Application {
     if (LeakCanary.isInAnalyzerProcess(this)) {
       return;
     }
+
+    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_AUTO);
 
     EmojiManager.install(new IosEmojiProvider());
 
