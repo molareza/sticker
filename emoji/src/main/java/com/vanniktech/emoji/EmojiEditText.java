7464648cc1e13139e497aa56a34f1b1549a880a1
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-06-24 20:13:09 +0200

    Add back Emoji Size and use line height as the default. (#146)

--- emoji/src/main/java/com/vanniktech/emoji/EmojiEditText.java
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiEditText.java
@@ -1,13 +1,17 @@
 package com.vanniktech.emoji;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.CallSuper;
+import android.support.annotation.Px;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import com.vanniktech.emoji.emoji.Emoji;
 
 public class EmojiEditText extends AppCompatEditText {
+  private int emojiSize;
+
   public EmojiEditText(final Context context) {
     this(context, null);
   }
@@ -20,10 +24,22 @@ public class EmojiEditText extends AppCompatEditText {
     }
 
     setText(getText());
+
+    if (attrs == null) {
+      emojiSize = getLineHeight();
+    } else {
+      final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.emoji);
+
+      try {
+        emojiSize = (int) a.getDimension(R.styleable.emoji_emojiSize, getLineHeight());
+      } finally {
+        a.recycle();
+      }
+    }
   }
 
   @Override @CallSuper protected void onTextChanged(final CharSequence text, final int start, final int lengthBefore, final int lengthAfter) {
-    EmojiHandler.replaceWithImages(getContext(), getText(), getLineHeight());
+    EmojiHandler.replaceWithImages(getContext(), getText(), emojiSize);
   }
 
   @CallSuper public void backspace() {
@@ -42,5 +58,10 @@ public class EmojiEditText extends AppCompatEditText {
         getText().replace(Math.min(start, end), Math.max(start, end), emoji.getUnicode(), 0, emoji.getUnicode().length());
       }
     }
+  }
+
+  public void setEmojiSize(@Px final int pixels) {
+    emojiSize = pixels;
+    setText(getText()); // Update it.
   }
 }
--- emoji/src/main/java/com/vanniktech/emoji/EmojiTextView.java
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiTextView.java
@@ -1,12 +1,16 @@
 package com.vanniktech.emoji;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.CallSuper;
+import android.support.annotation.Px;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.SpannableStringBuilder;
 import android.util.AttributeSet;
 
 public class EmojiTextView extends AppCompatTextView {
+  private int emojiSize;
+
   public EmojiTextView(final Context context) {
     this(context, null);
   }
@@ -19,12 +23,29 @@ public class EmojiTextView extends AppCompatTextView {
     }
 
     setText(getText());
+
+    if (attrs == null) {
+      emojiSize = getLineHeight();
+    } else {
+      final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.emoji);
+
+      try {
+        emojiSize = (int) a.getDimension(R.styleable.emoji_emojiSize, getLineHeight());
+      } finally {
+        a.recycle();
+      }
+    }
   }
 
   @Override @CallSuper public void setText(final CharSequence rawText, final BufferType type) {
     final CharSequence text = rawText == null ? "" : rawText;
     final SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(text);
-    EmojiHandler.replaceWithImages(getContext(), spannableStringBuilder, getLineHeight());
+    EmojiHandler.replaceWithImages(getContext(), spannableStringBuilder, emojiSize);
     super.setText(spannableStringBuilder, type);
+  }
+
+  public void setEmojiSize(@Px final int pixels) {
+    emojiSize = pixels;
+    setText(getText()); // Update it.
   }
 }
