b90230500df3fd4e088735d97f655709b3721e67
molareza <a.molareza@gmail.com>
2018-12-25 11:44:53 -0800

    a2

---
+++ generator/template/Category.java
@@ -0,0 +1,21 @@
+package com.vanniktech.emoji.<%= package %>.category;
+
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import com.vanniktech.emoji.emoji.EmojiCategory;
+import com.vanniktech.emoji.<%= package %>.R;
+import com.vanniktech.emoji.<%= package %>.<%= name %>;
+
+@SuppressWarnings("PMD.MethodReturnsInternalArray") public final class <%= category %>Category implements EmojiCategory {
+  private static final <%= name %>[] DATA = new <%= name %>[] {
+    <%= data %>
+  };
+
+  @Override @NonNull public <%= name %>[] getEmojis() {
+    return DATA;
+  }
+
+  @Override @DrawableRes public int getIcon() {
+    return R.drawable.emoji_<%= package %>_category_<%= icon %>;
+  }
+}
